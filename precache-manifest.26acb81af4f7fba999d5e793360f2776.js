6d320c7ed72b50c7227445a0e0f4be0b13f3f02c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-04 16:56:53 +0000

    Deploy allenroberts1/HackerNewsCSC496 to github.com/allenroberts1/HackerNewsCSC496.git:gh-pages

---
+++ precache-manifest.26acb81af4f7fba999d5e793360f2776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6206ee04650ae2836ba1ba2f840f5f2",
+    "url": "/HackerNewsCSC496/index.html"
+  },
+  {
+    "revision": "24057c69265508bddcca",
+    "url": "/HackerNewsCSC496/static/css/main.3eb6be07.chunk.css"
+  },
+  {
+    "revision": "e05b1aabcca980f5f219",
+    "url": "/HackerNewsCSC496/static/js/2.d1705005.chunk.js"
+  },
+  {
+    "revision": "ad4f66d4e4d5958174574764425c2377",
+    "url": "/HackerNewsCSC496/static/js/2.d1705005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24057c69265508bddcca",
+    "url": "/HackerNewsCSC496/static/js/main.3c6229b5.chunk.js"
+  },
+  {
+    "revision": "09f7aa0cc8dd00842170",
+    "url": "/HackerNewsCSC496/static/js/runtime-main.5ef8b124.js"
+  }
+]);
